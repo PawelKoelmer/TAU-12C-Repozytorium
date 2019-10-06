b2457724ab51d1b81006a5f52c06a25622e315d3
Pawelk <pawel.koelmer@gmail.com>
2019-10-06 12:14:06 +0200

    Add Tests for Creating List, AddObject and get all elements

--- src/main/java/baza/slodycz/BazaSlodyczy.java
+++ src/main/java/baza/slodycz/BazaSlodyczy.java
@@ -1,4 +1,25 @@
 package baza.slodycz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BazaSlodyczy {
+
+    ArrayList<Slodycz> slodycze = new ArrayList();
+    Slodycz slodycz;
+
+    public void insert(Slodycz slodycz) {
+            slodycze.add(slodycz);
+    }
+
+    public List<Slodycz> getAll() {
+        return slodycze;
+    }
+
+    public Slodycz getSingle(int id) {
+
+        slodycz = slodycze.get(id);
+
+        return slodycz;
+    }
 }
--- src/test/java/baza/slodycz/SlodyczTest.java
+++ src/test/java/baza/slodycz/SlodyczTest.java
@@ -1,23 +1,42 @@
 package baza.slodycz;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
 
+@RunWith(JUnit4.class)
 public class SlodyczTest {
 
     @Test
     public void createObjectTest(){
-
         assertNotNull(new BazaSlodyczy());
-    }
-    
+}
+
     @Test
     public void createSingleObjectTest(){
         Slodycz slodycz = new Slodycz(1,"Czekolada","Słodka");
-        assertEquals(1,slodycz.getId());
+        assertEquals(1, slodycz.getId());
         assertEquals("Czekolada",slodycz.getNazwa());
         assertEquals("Słodka",slodycz.getOpis());
     }
+
+    @Test
+    public void addObjectToDatabase(){
+        BazaSlodyczy baza = new BazaSlodyczy();
+        Slodycz slodycz = new Slodycz(1,"Czekolada","Słodka");
+        baza.insert(slodycz);
+        assertEquals(1,baza.getSingle(0).getId());
+    }
+
+    @Test
+    public void getListOfObjectsFromDatabaseNotNull(){
+        BazaSlodyczy baza = new BazaSlodyczy();
+        assertNotNull(baza.getAll());
+    }
+
+
+
 }
